54359d60b59a8c3122352a2668c9c23b68ade445
valeroman <valeraromanovo@gmail.com>
2022-11-14 11:23:02 +0300

    Complete 3d task of JS Exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = '';
+    let strLength = string.length;
+    for (let i = strLength - 1; i >= 0; i--) {
+        reversedString += string[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
