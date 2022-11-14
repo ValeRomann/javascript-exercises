0359ef16309e9703b4680656a22a94b4c674fee2
valeroman <valeraromanovo@gmail.com>
2022-11-14 13:49:03 +0300

    Complete 8th task of JS Exercices

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let sum = 1;
+  for (let i = 0; i < arr.length; i++) {
+    sum *= arr[i];
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let sum = a;
+  for (let i = 1; i < b; i++) {
+    sum *= a;
+  }
+  return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a < 1) return 1;
+	let sum = 1;
+  for (let i = 1; i <= a; i++) {
+    sum *= i
+  }
+  return sum;
 };
 
 // Do not edit below this line
