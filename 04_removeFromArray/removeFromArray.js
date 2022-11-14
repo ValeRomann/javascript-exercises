3aaa2428a490416769b5513a91974eb74e2f38d1
valeroman <valeraromanovo@gmail.com>
2022-11-14 11:51:07 +0300

    Complete 4th task of JS Exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...index) {
+    let tempArr = arr;
+    for (let i = 0; i < tempArr.length; i++) {
+        for (let j = 0; j < index.length; j++) {            
+            if (tempArr[i] === index[j]) {
+                tempArr.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return tempArr;
 };
 
 // Do not edit below this line
