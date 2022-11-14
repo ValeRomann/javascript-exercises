e5ef2907193e8548c3a6241ff8c7366d36290ac8
valeroman <valeraromanovo@gmail.com>
2022-11-14 15:06:02 +0300

    Complete 11th Task of JS Exercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let titlesArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        titlesArr.push(arr[i]['title']);
+    }
+    return titlesArr;
 };
 
 // Do not edit below this line
