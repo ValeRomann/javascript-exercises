2fa69fabc4f84e85f6c2859d8e29789a485773ce
valeroman <valeraromanovo@gmail.com>
2022-11-14 15:01:37 +0300

    Complete 10th task of JS Exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 1) return 'OOPS';
 
+    let a = 1;
+    let b = 1;
+    let fib = 1;
+    
+    for (let i = 2; i < num; i++) {
+        fib = a + b;
+        a = b
+        b = fib;
+    }
+
+    return fib;
 };
 
 // Do not edit below this line
