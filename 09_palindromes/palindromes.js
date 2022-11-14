a1a6a47f073b75fc8c5413913134ee29e89215e6
valeroman <valeraromanovo@gmail.com>
2022-11-14 14:23:54 +0300

    Complete 9th task of JS Exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    newString = string.toLowerCase();
+    let stringArr = [];
+    let reversedArr = [];
+    let pattern = /[a-z]/;
 
+    for (let i = 0; i < newString.length; i++) {
+        if (pattern.test(newString[i])) stringArr.push(newString[i]);
+    }
+
+    for (let i = stringArr.length - 1; i >= 0; i--) {
+        reversedArr.push(stringArr[i]);
+    }
+
+    for (let i = 0; i < stringArr.length; i++) {
+        if (stringArr[i] !== reversedArr[i]) return false;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
