2e510ee9d7c62ec0e2711ab3f3168c42bea600ea
valeroman <valeraromanovo@gmail.com>
2022-11-14 12:24:23 +0300

    Complete 5th task of JS Exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0) return 'ERROR';
+    if (typeof start !== 'number' && isFinite(start) ||
+     typeof end !== 'number'  && isFinite(start)) return 'ERROR';
+    let sum = 0;
+    let small = start > end ? end : start;
+    let big = start > end ? start : end;
+    for (let i = small; i <= big; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
