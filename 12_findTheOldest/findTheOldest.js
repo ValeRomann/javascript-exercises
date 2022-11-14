4f6e652933b8ba5eba0db28a1d074bd855907e18
valeroman <valeraromanovo@gmail.com>
2022-11-14 16:36:55 +0300

    Complete 12th task of JS Exerces

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldest = {};
+    let maxYears = 0;
+    for (let i = 0; i < arr.length; i++) {     
+        let deathYear = arr[i]['yearOfDeath'] ? arr[i]['yearOfDeath'] : new Date().getFullYear();
+        let currentYears = deathYear - arr[i]['yearOfBirth'];
+        if (currentYears > maxYears) {
+            maxYears = currentYears;
+            oldest = arr[i];
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
