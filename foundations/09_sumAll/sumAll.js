d80484f0a6ec5eaffbfe81a1a4627c7b51ac8c07
ValeRoman <46073200+ValeRomann@users.noreply.github.com>
2026-07-04 19:50:36 +0300

    Finish the exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(s, e) {
+    if (s < 0 || e < 0 || !Number.isInteger(s) || !Number.isInteger(e)) return 'ERROR';
+    let sum = 0;
+    if (e < s) [s, e] = [e, s];
+    for (let i = s; i <= e; i++) sum += i;
+    return sum;
 };
 
 // Do not edit below this line
